884a5833f2d531d5543c21b79b31780af7ef4aae
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2022-11-29 14:35:13 -0600

    cambios finales

--- src/app/ui/ui.component.spec.ts
+++ src/app/ui/ui.component.spec.ts
@@ -26,11 +26,11 @@ describe('Ui Addition - Component', () => {
      let result = 0;
      component.operator1 = 2;
      component.operator2 = 2;
- 
+
      // Act
      component.addition();
      result = component.result;
- 
+
      // Assert
      expect(result).toBe(4);
   });
@@ -38,65 +38,52 @@ describe('Ui Addition - Component', () => {
 
 
   it('Should set operator1 model through ngModel', async() => {
-    // Arrange 
+    // Arrange
     await fixture.whenStable();
     fixture.detectChanges();
     const inputElement = fixture.debugElement.query(By.css('input[name="operator1"]')).nativeElement;
 
-    // Act 
+    // Act
     inputElement.value = '3.1416';
     inputElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    // Assert 
+    // Assert
     expect(component.operator1).toEqual(3.1416);
   });
- 
+
   it('Should set operator2 model through ngModel', async() => {
-    // Arrange 
+    // Arrange
     await fixture.whenStable();
     fixture.detectChanges();
     const inputElement = fixture.debugElement.query(By.css('input[name="operator2"]')).nativeElement;
 
-    // Act 
+    // Act
     inputElement.value = '2.71';
     inputElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
 
-    // Assert 
+    // Assert
     expect(component.operator2).toEqual(2.71);
   });
 
 
-  it('should add operator1 and operator2 when i click the addition button ', () => {
-    // Arrange 
-    component.operator1 = 5.0;
-    component.operator2 = 2.5;
-    let additionButton = fixture.debugElement.query(By.css('.addition-button'));
-
-    // Act
-    additionButton.triggerEventHandler('click', null);
-
-    // Assert
-    expect(component.result).toBe(7.5);
-
-   });
 
   it('Should render sum in result div', () => {
     // Arrange
     component.operator1 = 5;
     component.operator2 = 5;
- 
+
     // Act
     component.addition();
     fixture.detectChanges();
-    
+
     let de = fixture.debugElement.query(By.css('.result'));
     let el : HTMLElement = de.nativeElement;
 
     // Assert
     expect(el.innerText).toContain('10');
-     
+
   });
 
 });
